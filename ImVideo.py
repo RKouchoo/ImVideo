e5d8da76ced527c75441a7bbaf625a69d798546b
Rick K <r.kouchoo1@gmail.com>
2023-10-06 13:59:24 +1100

    fixed lol
    fixed lol

--- ImVideo.py
+++ ImVideo.py
@@ -1,24 +1,19 @@
 import cv2
 import numpy as np
 import glob
-
-frame_delay = 15
-path = '/raw_images'
-name = 'latest_timelapse.avi'
+import os 
+path = input("path: ")
+os.chdir(path)
 
 img_array = []
+for filename in glob.glob('*.jpg'):
+   img = cv2.imread(filename)
+   height, width, layers = img.shape
+   size = (width,height)
+   img_array.append(img)
 
-for filename in glob.glob(path):
-    
-    img = cv2.imread(filename)
-    height, width, layers = img.shape
-    size = (width, height)
-    img_array.append(img)
- 
- 
-out = cv2.VideoWriter(name, cv2.VideoWriter_fourcc(*'DIVX'), frame_delay, size)
- 
+out = cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
+
 for i in range(len(img_array)):
-    out.write(img_array[i])
-
+   out.write(img_array[i])
 out.release()
